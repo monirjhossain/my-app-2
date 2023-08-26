25d81bc5d4098d6c6010a56c14d420b6f2de2e4b
unknown <md.monirjhossain@gmail.com>
2023-08-26 17:18:49 +0600

    Fetch Dynamic Data from API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternalUser></ExternalUser>
+      <User></User>
     </div>
   );
+}
+
+function ExternalUser(){
+  const [users, setUsers] = useState([]);
+  useEffect( () =>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res =>res.json())
+    .then(data => setUsers(data))
+  },[]);
+  return (
+    <div>
+      <h1>External Users</h1>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+      
+    </div>
+  );
+}
+
+function User(props){
+  return (
+    // console.log(props)
+    <div>
+      <h3>Name: {props.name}</h3>
+      <p>Email: {props.email}</p>
+    </div>
+  )
 }
 
 
@@ -18,7 +48,6 @@ function Counter(){
 
   const increaseCount = () => setCount(count + 1);
   const DecreaseCount = () => setCount(count - 1);
-    
   
 
   return (
